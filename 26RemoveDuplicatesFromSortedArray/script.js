2aaaa8fd8b35f43de44db212f2bbbbd58d8af19f
suveiko <suveikosasha@gmail.com>
2022-07-14 18:59:09 +0400

    feat: 26. Remove Duplicates from Sorted Array

---
+++ 26RemoveDuplicatesFromSortedArray/script.js
@@ -0,0 +1,13 @@
+var removeDuplicates = function(nums) {
+    const set = new Set(nums);
+    nums.length = 0;
+    nums.push(...set.values())
+    return set.size;
+};
+
+console.log(removeDuplicates([1,1,2]))
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+//     It does not matter what you leave beyond the returned k (hence they are underscores).
